44c9bd250f0d711a0ae02dad416750dbae016d36
Sharath Mekottil <sharath.m@tavant.com>
2018-01-28 12:14:28 +0530

    Moving jpa configuration to utility

---
+++ src/main/java/com/ionwallet/configuration/JPAConfiguration.java
@@ -0,0 +1,70 @@
+package com.ionwallet.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+
+@Configuration
+@PropertySource("classpath:database.properties")
+@EnableJpaRepositories(basePackages={"com.ionwallet"})
+public class JPAConfiguration {
+
+	@Value("${database.username}")
+	private String userName;
+
+	@Value("${database.password}")
+	private String password;
+	
+	@Value("${database.url}")
+	private String url;
+
+	@Value("${database.platform}")
+	private String databasePlatform;
+	
+
+	@Value("${database.hbm2ddl.auto}")
+	private String hbm2ddl;
+	
+	@Value("${packages.to.scan}")
+	private String packagesToScan;
+	
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername(userName);
+		dataSource.setPassword(password);
+		dataSource.setUrl(url);
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { packagesToScan });
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+
+		return em;
+	}
+
+	Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", hbm2ddl);
+		properties.setProperty("hibernate.dialect", databasePlatform);
+		return properties;
+	}
+}
